bcbd2e2f21c87628e56057e67c70befde7264172
Monzarllo Taylor <68361122+moetaylor2020@users.noreply.github.com>
2025-12-19 15:54:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+ 
+ alert("Welcome to Monzarllo's digital Drum Kit!");
+
+
+$("button").click(function() {
+    $("h1").css("color", "green");
+}); 
+
+
+
+
+//for (var i = 0; i<5; i++) {
+  //  document.querySelectorAll("button")[i].addEventListener("click", function() {
+   //     document.querySelector("h1").style.color ="purple";
+ //   })
+//}
+
+
+
+
+
+
+//$("h1").text("Bye");
+
+//$("button").text("<em>hey</em>");
+
+// $(document).ready(function() {
+// $("h1").css("color", "red");
+ })
